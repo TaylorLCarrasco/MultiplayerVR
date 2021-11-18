57acf370b29a25fc6e90bddb1779c8375f288ce5
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-18 02:10:21 -0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -6,6 +6,32 @@ window.addEventListener("load", () => {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
   
-  //ctx.fillStyle = "red";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //Variables
+  let painting = false;
+  
+  function startPosition(){
+    painting = true;
+    draw(e);
+  }
+  
+  function finishedPosition(){
+    painting = false;
+    ctx.beginPath();
+  }
+  
+  function draw(e){
+    if(!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = "round";
+    
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+  }
+  
+  //EventListeners
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
 });
