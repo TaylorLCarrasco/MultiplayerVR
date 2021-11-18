dc3e578222305d005d1e3bfb04f7a5925158095f
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-18 01:41:33 -0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -3,8 +3,8 @@ window.addEventListener("load", () => {
   const ctx = canvas.getContext("2d");
   
   //Resizing
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+  //canvas.height = window.innerHeight;
+  //canvas.width = window.innerWidth;
   
   ctx.strokeStyle = "red";
   ctx.strokeRect(0, 0, canvas.width, canvas.height);
