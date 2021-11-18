2df3595728f24fa1c0d2828c24b1714ad7e67e27
TaylorLCarrasco <50971474+TaylorLCarrasco@users.noreply.github.com>
2021-11-18 01:21:42 -0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,8 @@
-
+window.addEventListener('load', () => {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+  
+  //Resizing
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerwidth;
+});
